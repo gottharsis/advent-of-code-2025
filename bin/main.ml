af843ea2e3a7718a9e40ba4ed0dd74215de203ed
anene <anene@bloomberg.net>
2025-12-05 12:49:57 -0500

    more boilerplate

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,19 @@
-let () = print_endline "Hello, World!"
+let usage_message = "aoc [-t] <day-number>"
+let use_test_input = ref false
+let day_num = ref 0
+
+let anon_fun day =
+  day_num := int_of_string day
+
+let invalid_day day =
+  Printf.eprintf "Day %d not implemented yet\n" day;
+  exit 1
+
+let speclist =
+  [("-t", Arg.Set use_test_input, "Use test input")]
+
+let () =
+  Arg.parse speclist anon_fun usage_message;
+  match !day_num with
+  | x -> invalid_day x
+
